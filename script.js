feea4889924d7a87120939b78dde0a397a1b1680
MaulanaAkbar <100104033+MaulanaAkbarF@users.noreply.github.com>
2023-10-11 20:28:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-<script>
-  const text = "Aplikasi Pengolah Citra dan Vision";
-  let charIndex = 0;
-  let timer;
+const text = "Aplikasi Pengolah Citra dan Vision";
+let charIndex = 0;
 
-  function type() {
-    document.getElementById("typerwriter-text").textContent = text.slice(0, charIndex++);
-    if (charIndex <= text.length) {
-      timer = setTimeout(type, 100); // Waktu jeda antar karakter (milidetik)
-    }
+function type() {
+  const element = document.getElementById("typerwriter-text");
+  if (charIndex <= text.length) {
+    element.textContent = text.slice(0, charIndex);
+    charIndex++;
+    setTimeout(type, 100); // Waktu jeda antar karakter (milidetik)
   }
+}
 
-  // Mulai animasi ketika halaman dimuat
-  window.addEventListener("load", () => {
-    type();
-  });
-</script>
+// Jalankan animasi ketika dokumen dimuat
+document.addEventListener("DOMContentLoaded", type);
